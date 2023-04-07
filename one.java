518e2a08a6c9e2f239d52fa5de3a85546d5d5b34
Bhaskar Pandya <Vision@HARSH-2003>
2023-04-07 15:40:10 +0530

    addition program

--- one.java
+++ one.java
@@ -1,7 +1,11 @@
 // Your First Program
 
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("hi this is my first java program"); 
-    }
-}
+public class SumOfNumbers1  
+{  
+public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115, sum;  
+sum = n1 + n2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+}  
