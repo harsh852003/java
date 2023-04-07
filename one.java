fa57c615f8f4cadcb236c92679356015e2c7acf2
Bhaskar Pandya <Vision@HARSH-2003>
2023-04-07 15:28:18 +0530

    this is my first java program

--- one.java
+++ one.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("hi this is my first java program"); 
     }
 }
